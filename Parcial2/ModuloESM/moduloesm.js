3ad11466c969e21ed654d2a9fee92fa7e3f273db
YazminLMH <MyriamYazmin08@outlook.com>
2022-10-12 22:13:23 -0500

    moduloESM

---
+++ Parcial2/ModuloESM/moduloesm.js
@@ -0,0 +1,21 @@
+
+
+//const pasarMayusculas = (cadenas) =>
+export function pasarMayusculas(cadena){
+    return cadena.toUpperCase();
+}
+export function quitarEspacios(cadena) {
+    return cadena.replace(/ /g, "");
+
+}
+
+export function obtenerLongitud(cadena){
+    return cadena.length;
+}
+
+
+
+//npm init 
+//abrir cuenta en el registro de npm  
+// npm login 
+//npm publish 
---
+++ Parcial2/modulobalin/modulobalin.js
@@ -0,0 +1,27 @@
+
+
+//const pasarMayusculas = (cadenas) =>
+function pasarMayusculas(cadena){
+    return cadena.toUpperCase();
+}
+function quitarEspacios(cadena){
+    return cadena.trim();
+
+}
+
+function obtenerLongitud(cadena){
+    return cadena.length;
+}
+
+console.log(module);
+
+exports.pasarMayusculas = pasarMayusculas;
+exports.quitarEspacios = quitarEspacios;
+exports.obtenerLongitud = obtenerLongitud;
+
+console.log(module);
+
+//npm init 
+//abrir cuenta en el registro de npm  
+// npm login 
+//npm publish 
